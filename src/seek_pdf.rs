654ee2332965ba787ace97fe5668083b6d25d862
JD <dummy@dummy.com>
2024-06-16 11:35:54 +0900

    フルパス生成処理の場所をstart()からPDF検索処理に移動した。

--- src/seek_pdf.rs
+++ src/seek_pdf.rs
@@ -3,53 +3,60 @@ use std::{
     path::{Path, PathBuf},
 };
 
-/// `seek_pdf_file`関数は、指定されたディレクトリ内のすべてのPDFファイルを探し、そのパスを返します。
+use crate::check_path::is_valid_directory;
+
+/// # 概要
+/// この関数は指定されたディレクトリ内のPDFファイルを探し、そのパスのリストを返します。
 ///
 /// # 引数
-///
-/// * `directory_path` - PDFファイルを探すディレクトリのパス。`Path`参照として与えられます。
+/// * `directory_path`: PDFファイルを探すディレクトリのパスを指定します。
 ///
 /// # 戻り値
-///
-/// * 成功した場合は、PDFファイルのパスを含む`Vec<PathBuf>`を`Ok`でラップして返します。
-/// * ディレクトリの読み取りに失敗した場合は、エラーを`Err`でラップして返します。
+/// PDFファイルが見つかった場合はそのパスのリストを返します。
+/// 見つからなかった場合は空のリストを返します。
+/// ディレクトリが無効な場合はエラーを返します。
 ///
 /// # 例
-///
 /// ```
-/// let path = Path::new("/some/directory");
-/// let result = seek_pdf_file(&path);
-/// assert!(result.is_ok());
+/// let result = seek_pdf_file(Path::new("/path/to/directory"));
+/// match result {
+///     Ok(paths) => for path in paths {
+///         println!("Found PDF at: {}", path.display());
+///     },
+///     Err(e) => println!("An error occurred: {}", e),
+/// }
 /// ```
 ///
-/// # 注意
-///
-/// この関数はファイルシステムにアクセスするため、ディレクトリの読み取りには適切なパーミッションが必要です。
-
 pub fn seek_pdf_file(directory_path: &Path) -> std::io::Result<Vec<std::path::PathBuf>> {
+    let src_dir: std::path::PathBuf = match is_valid_directory(directory_path) {
+        Ok(path) => path,
+        Err(e) => return Err(e),
+    };
     let mut pdf_files = Vec::new();
-
-    if directory_path.is_dir() {
-        for entry in fs::read_dir(directory_path)? {
-            let entry: fs::DirEntry = entry?;
-            let path: PathBuf = entry.path();
-            if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("pdf") {
-                pdf_files.push(path);
-            }
+    for entry in fs::read_dir(src_dir.as_path())? {
+        let entry: fs::DirEntry = entry?;
+        let path: PathBuf = entry.path();
+        if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("pdf") {
+            pdf_files.push(path);
         }
     }
-
     Ok(pdf_files)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_seek_pdf_file_empty_directory() {
-        let empty_dir = Path::new("test_pdf/empty_dir");
-        let pdf_files = seek_pdf_file(empty_dir).unwrap();
+        let dir_str: &str = "test_pdf/empty_dir";
+        let dmpty_dir_path = Path::new(dir_str);
+        //pdf_file_pathのディレクトリが存在しない場合は作成する。作成に失敗した場合はpanicする。
+        fs::create_dir_all(dmpty_dir_path).expect("COULD NOT MAKE DIRECTORY.");
+        let pdf_files = seek_pdf_file(dmpty_dir_path).unwrap();
         assert_eq!(pdf_files.len(), 0);
+        //pdf_file_pathのディレクトリが存在している場合は削除する。削除に失敗した場合はpanicする。
+        fs::remove_dir_all(dmpty_dir_path).expect("COULD NOT REMOVE DIRECTORY.");
     }
 
     #[test]
@@ -58,16 +65,21 @@ mod tests {
 
         let mut pdf_file1: PathBuf = temp_dir.clone();
         pdf_file1.push("file1.pdf");
+        let full_path_pdf_file1 = fs::canonicalize(pdf_file1.as_path()).unwrap();
+
         let mut pdf_file2: PathBuf = temp_dir.clone();
         pdf_file2.push("file2.pdf");
+        let full_path_pdf_file2 = fs::canonicalize(pdf_file2.as_path()).unwrap();
+
         let mut pdf_file3: PathBuf = temp_dir.clone();
         pdf_file3.push("file3.pdf");
+        let full_path_pdf_file3 = fs::canonicalize(pdf_file3.as_path()).unwrap();
 
         let pdf_files: Vec<std::path::PathBuf> = seek_pdf_file(temp_dir.as_path()).unwrap();
         assert_eq!(pdf_files.len(), 3);
-        assert!(pdf_files.contains(&pdf_file1));
-        assert!(pdf_files.contains(&pdf_file2));
-        assert!(pdf_files.contains(&pdf_file3));
+        assert!(pdf_files.contains(&full_path_pdf_file1));
+        assert!(pdf_files.contains(&full_path_pdf_file2));
+        assert!(pdf_files.contains(&full_path_pdf_file3));
     }
 
     #[test]
